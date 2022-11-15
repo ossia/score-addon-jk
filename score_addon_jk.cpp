4eb771369e4d73f709d78c096fe521c435ba8407
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-11-14 19:02:32 -0500

    [ci] Update with score

--- score_addon_jk.cpp
+++ score_addon_jk.cpp
@@ -1,15 +1,13 @@
 #include "score_addon_jk.hpp"
 
-#include <score/plugins/FactorySetup.hpp>
-
 #include <Avnd/Factories.hpp>
 #include <Jk/Filter.hpp>
+#include <score/plugins/FactorySetup.hpp>
 #include <score_plugin_engine.hpp>
 
 static_assert(oscr::is_variant<jk::value>::value);
 static_assert(oscr::is_variant_vector<std::vector<jk::value>>::value);
-static_assert(
-    oscr::is_variant_list_vector<std::vector<std::vector<jk::value>>>::value);
+static_assert(oscr::is_variant_list_vector<std::vector<std::vector<jk::value>>>::value);
 // static_assert(boost::mp11::mp_find_if<oscr::is_variant_list_vector, jk::variant>>);
 /**
  * This file instantiates the classes that are provided by this plug-in.
@@ -18,10 +16,11 @@ score_addon_jk::score_addon_jk() = default;
 score_addon_jk::~score_addon_jk() = default;
 
 std::vector<std::unique_ptr<score::InterfaceBase>> score_addon_jk::factories(
-    const score::ApplicationContext& ctx,
-    const score::InterfaceKey& key) const
+    const score::ApplicationContext& ctx, const score::InterfaceKey& key) const
 {
-  return Avnd::instantiate_fx<Jk::Filter>(ctx, key);
+  std::vector<std::unique_ptr<score::InterfaceBase>> fx;
+  Avnd::instantiate_fx<Jk::Filter>(fx, ctx, key);
+  return fx;
 }
 
 std::vector<score::PluginKey> score_addon_jk::required() const
